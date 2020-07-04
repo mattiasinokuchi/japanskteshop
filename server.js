e9319f8c1847563afafa1e038bbd63401e0f4ad4
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2020-07-04 11:39:15 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express'); //load express
+const hbs = require('hbs'); //load handlebars
+const fs = require('fs');
+const app = express();  //make a new express app
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+app.set('view engine', 'hbs');  //declare view engine
+app.use((req, res, next) => { //register middleware to serve a logger
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+app.use(express.static(__dirname + '/public')); //add middleware to serve static HTML pages
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Japansk Teshop',
+    welcomeMessage: 'Välkommen till min nya affär!!!',
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'Om Japansk Teshop',
+  });
+});
+app.listen(3000, () => {  //bind app to port 3000
+  console.log('Server is up on port 3000');
+});
